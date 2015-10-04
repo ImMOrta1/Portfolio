dd792ba36da0f0d3df7451ddd0445567cc52cf3f
ImM0rta1 <webart@bk.ru>
2015-10-04 21:08:13 +0300

    Popup beta

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
-jQuery(document).ready(function($) {
-	console.log('правильное подключение')
-});
+$(document).ready(function(){
+
+	$('.add-project-link').on('click', function(e) {
+		e.preventDefault();
+
+		$('.popup').bPopup({
+			closeClass : 'popup-close'
+		});
+	});
+
+	$('.popup-input-file-upload').on('change', function(){
+
+		var
+			$this = $(this),
+			value = $this.val(),
+			pureVal = value.replace(/c:\\fakepath\\/gmi, "");
+
+		$('.popup-upload-fake').text(pureVal);
+
+	});
+}); // - > ready_end;
